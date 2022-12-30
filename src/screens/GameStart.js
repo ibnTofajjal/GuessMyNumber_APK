751dfd19f7328117fa7e14abb97978bdabdf3e82
Joynal Bin Tofajjal <joynal.bintofajjal@gmail.com>
2022-12-30 20:34:58 +0600

    Style Input And added shadow on IOS and also on Android system

--- src/screens/GameStart.js
+++ src/screens/GameStart.js
@@ -1,12 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 import React from "react";
 import PrimaryButton from "../components/PrimaryButton";
 
 export default function GameStart() {
   return (
-    <View>
+    <View style={styles.InputContainer}>
+      <TextInput placeholder="Number" />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  InputContainer: {
+    backgroundColor: "#72063c",
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 20,
+    // Android shadow
+    elevation: 15,
+    // IOS shadow
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.26,
+    shadowRadius: 6,
+    borderRadius: 16,
+  },
+});
